67611e9d7cc30b7c73fe54dab571ab38f240bfc0
tararoskell <tara@roskelldesign.co.uk>
2013-04-29 17:34:08 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onemonthrails2::Application.config.secret_token = '3efc1f1f5512597cfc14178f826dc13301b47e842d03cac7d532c02f3673519c1ee52ecc537af87986140525fb625667145ac35e9137dbd3a3412a701ae9464d'
